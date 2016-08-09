2c219440b59908277a3727f5b0b788a8b97405c0
Colin O'Sullivan <osullivancj@gmail.com>
2016-08-08 20:28:13 -0700

    Update local.js

--- public/local.js
+++ public/local.js
@@ -24,7 +24,7 @@ socket.on('chat', function(data){
 function sendMessage(event) {
 	console.log('SENDING: name: '+ nameInput.value + ', message: ' + messageInput.value);
 	socket.emit('chat', {name: nameInput.value, message: messageInput.value} );  
-	messageInput = '';
+	messageInput.value = '';
 }
 
 function displayNewMessage (username, message) {
